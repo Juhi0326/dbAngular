32134c3f1e64b8aa796efd9f637ddcfd1a24a429
juhi <juhi0326@gmail.com>
2019-04-14 14:36:52 +0200

    create customer component and customer service

---
+++ src/app/Services/customer.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { CustomersComponent } from './../customers/customers.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor() { }
+}
