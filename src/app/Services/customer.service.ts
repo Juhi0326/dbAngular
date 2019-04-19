9ef8c3e44541e2a120c360444b6227efd07d499e
juhi <juhi0326@gmail.com>
2019-04-19 16:16:16 +0200

    snapShotChanges bevezetés, Id megszerzése az adatbázisból

--- src/app/Services/customer.service.ts
+++ src/app/Services/customer.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
-
+import { CustModell } from './../cust-modell';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -14,10 +14,17 @@ export class CustomerService {
 
 
   constructor(private db: AngularFirestore) {
-    this.items = this.db.collection('proba').valueChanges()
+    //this.items = this.db.collection('proba').valueChanges();
+    this.itemsCollection = this.db.collection<CustModell>('proba');
+    this.items = this.db.collection('proba').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as CustModell;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
   }
   getCustomers() {
     return this.items;
   }
 }
-
