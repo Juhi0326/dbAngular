9ae880a30ba7e42601c9232680c0a89a00c7007a
juhi <juhi0326@gmail.com>
2019-07-15 07:50:18 +0200

    finish base reactive form for add-item component using built in validators

--- src/app/customers/add-item/add-item.component.ts
+++ src/app/customers/add-item/add-item.component.ts
@@ -31,34 +31,45 @@ export class AddItemComponent implements OnInit {
   ngOnInit() {
     this.addItemForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
-      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
-      age: [0, [Validators.required, Validators.min(18), Validators.max(120)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
+      age: [0, [Validators.required, Validators.min(18), Validators.max(120), Validators.pattern(/^\d{1,3}$/)]],
       email: ['', [Validators.required, Validators.email]],
       gender: ['', [Validators.required]],
       fullTime: [true, [Validators.required]],
-      yearsOfExperience: [0, [Validators.required, Validators.max(80)]]
+      yearsOfExperience: [0, [Validators.required, Validators.max(80), Validators.pattern(/^\d{1,2}$/)]]
     });
   }
 
   onSubmit() {
 
-    if (this.item.age > 0 && this.item.email !== '' &&
-      this.item.fName !== '' && this.item.gender !== '' &&
-      this.item.lName !== '') {
+    if (this.addItemForm.valid) {
+      this.item.age = this.addItemForm.get('age').value;
+      this.item.email = this.addItemForm.get('email').value;
+      this.item.fName = this.addItemForm.get('firstName').value;
+      this.item.lName = this.addItemForm.get('lastName').value;
+      this.item.gender = this.addItemForm.get('gender').value;
+      this.item.isFullTime = this.addItemForm.get('fullTime').value;
+      this.item.yearsOfExperience = this.addItemForm.get('yearsOfExperience').value;
       this.customerService.addCustomer(this.item);
-      this.item.age = 0;
-      this.item.email = '';
-      this.item.fName = '';
-      this.item.lName = '';
-      this.item.gender = '';
-      this.item.isFullTime = false;
-      this.item.yearsOfExperience = 0;
+      this.clear();
       this.goBack();
+    } else {
+      console.log('the form is invalid');
     }
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  clear(): void {
+    this.item.age = 0;
+    this.item.email = '';
+    this.item.fName = '';
+    this.item.lName = '';
+    this.item.gender = '';
+    this.item.isFullTime = false;
+    this.item.yearsOfExperience = 0;
   }
 
 }
