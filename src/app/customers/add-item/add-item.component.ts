07d9ab1c974e7c6630788e2a0a18aa5af7ac86e4
juhi <juhi0326@gmail.com>
2019-07-21 16:05:41 +0200

    validate radio button in add-item form

--- src/app/customers/add-item/add-item.component.ts
+++ src/app/customers/add-item/add-item.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CustModell } from './../cust-modell';
-import { Location } from '@angular/common';
 import { CustomerService } from './../customer.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from './../../shared/messages/message.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-item',
@@ -22,20 +22,21 @@ export class AddItemComponent implements OnInit {
     lName: '',
     yearsOfExperience: 0
   };
+  isSubmitted: boolean;
 
   constructor(
     private customerService: CustomerService,
-    private location: Location,
     private fb: FormBuilder,
-    public ms: MessageService
+    public ms: MessageService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.addItemForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40),
-      Validators.pattern(/^[a-zA-ZáéíóőúűÁÉÍÓŐÚŰ]+$/)]],
+      Validators.pattern(/^[a-zA-ZáéíóöőüúűÁÉÍÓÖŐÚŰ]+$/)]],
       lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40),
-      Validators.pattern(/^[a-zA-ZáéíóőúűÁÉÍÓŐÚŰ]+$/)]],
+      Validators.pattern(/^[a-zA-ZáéíóöőüúűÁÉÍÓÖŐÚŰ]+$/)]],
       age: [0, [Validators.required, Validators.min(18), Validators.max(120), Validators.pattern(/^\d{1,3}$/)]],
       email: ['', [Validators.required, Validators.email]],
       gender: ['', [Validators.required]],
@@ -45,8 +46,10 @@ export class AddItemComponent implements OnInit {
   }
 
   onSubmit() {
+    this.ms.clear();
 
     if (this.addItemForm.valid) {
+
       this.item.age = this.addItemForm.get('age').value;
       this.item.email = this.addItemForm.get('email').value;
       this.item.fName = this.addItemForm.get('firstName').value;
@@ -55,15 +58,19 @@ export class AddItemComponent implements OnInit {
       this.item.isFullTime = this.addItemForm.get('fullTime').value;
       this.item.yearsOfExperience = this.addItemForm.get('yearsOfExperience').value;
       this.customerService.addCustomer(this.item);
+      this.isSubmitted = true;
       this.clear();
       this.goBack();
     } else {
+      this.isSubmitted = true;
       this.ms.addMessage('The form is invalid!');
+      this.router.navigate([{ outlets: { popup: ['messages'] } }]);
     }
   }
 
+
   goBack(): void {
-    this.location.back();
+    this.router.navigate(['/customers']);
   }
 
   clear(): void {
