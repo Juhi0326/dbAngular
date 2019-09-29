6a5bb1e348454adf756f4dd76c38ddbd078113f0
juhi <juhi0326@gmail.com>
2019-09-29 14:17:28 +0200

    add CUSTOM_ELEMENTS_SCHEMA, and fix a missing </div> tag

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { slideInAnimation } from './animation';
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [slideInAnimation],
       declarations: [
         AppComponent
       ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
